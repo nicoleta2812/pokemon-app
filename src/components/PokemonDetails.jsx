1b8b6fd6d3e020a033e59637dd26f48d06de315f
NicoletaBarbacaru <nicoletaburlacu28@yahoo.com>
2024-07-26 16:16:39 +0300

    fetched data and displayed for PokemonDetails component

--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -1,8 +1,31 @@
-function PokemonDetails(){
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom" 
+
+function PokemonDetails({ }){
+    const [details, setDetails] = useState("");
+    const { pokemonName } = useParams();
+    console.log(pokemonName);
+
+    useEffect(()=>{
+        async function fetchDetails(){
+            const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`);
+            const data = await res.json();
+            // console.log(data);
+            setDetails(data);
+        } 
+        fetchDetails()
+    }, [pokemonName])
+
     return (
-        <div>
+        <div className="pokemonDetailsCard">
             <h1>Pokemon Details</h1>
-        </div>
+            <h2>{details.name}</h2>
+            <div>{details.height}</div>            
+            <div>
+               <img src={details.sprites.other.front_default} width="150px"></img>
+               <img src={details.sprites.other.back_default} width="150px"></img>
+            </div>    
+            </div>
     )
 }
 export default PokemonDetails;
