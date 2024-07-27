2f1713aa7a76766e8a5a72e2ad4ad1c7f1a54abd
NicoletaBarbacaru <nicoletaburlacu28@yahoo.com>
2024-07-27 12:00:08 +0300

    displayed pokemon type in the PokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,27 +1,42 @@
-function PokemonCard({ details }) {
-    const { name, height, sprites } = details;
+import pokemonType from "../PokemonTypes";
 
-    return (
-        <div className="pokemonDetailsCard">
-            <h2>
-                {name}
-            </h2>
-            <div>Height: { height }</div>
-            <div>
-                <div className="pokemon-img">
-                    <img src={sprites?.other.home.front_default} alt={name} width="200px"></img>
-                </div>
-                <div>
-                    <img src={sprites.front_default} alt={name} />
-                    <img src={sprites.back_default} alt={name} />
-                    <img src={sprites.front_shiny} alt={name} />
-                    <img src={sprites.back_default} alt={name} />
-                </div>
-                <div>
-                    
-                </div>
-            </div>
+function PokemonCard({ details }) {
+  const { name, height, sprites, types } = details;
+  console.log(types);
+  return (
+    <div className="pokemonDetailsCard">
+      <h2>{name}</h2>
+      <div>Height: {height}</div>
+      <div>
+        <div className="pokemon-img">
+          <img
+            src={sprites?.other.home.front_default}
+            alt={name}
+            width="200px"
+          ></img>
         </div>
-    )
+        <div>
+          <img src={sprites?.front_default} alt={name} />
+          <img src={sprites?.back_default} alt={name} />
+          <img src={sprites?.front_shiny} alt={name} />
+          <img src={sprites?.back_shiny} alt={name} />
+        </div>
+        <div>
+          <h2>Types</h2>
+          <div className="types">
+            {types?.map((type) => (
+              // eslint-disable-next-line react/jsx-key
+              <div
+                className="card-type"
+                style={{ backgroundColor: pokemonType[type.type.name] }}
+              >
+                {type.type.name}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default PokemonCard
+export default PokemonCard;
